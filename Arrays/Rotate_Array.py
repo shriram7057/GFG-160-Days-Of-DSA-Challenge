77429a278fd5d271971708a0b0f8b64b70edf998
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-11-30 11:33:00 -0500

    Delete Arrays directory

--- Arrays/Rotate_Array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    #Function to rotate an array by d elements in counter-clockwise direction. 
-    def rotateArr(self, arr, d):
-        n = len(arr)
-        d = d % n  # Handle cases where d > n
-        arr[:] = arr[d:] + arr[:d]
--- Arrays/Stock Buy and Sell.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-class Solution:
-    def maximumProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i - 1]:
-                profit += prices[i] - prices[i - 1]
-        return profit
